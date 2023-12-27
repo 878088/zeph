dfbf6537fde62c140d9acb1c815074f41954b09f
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-27 08:15:23 +0800

    Update setup_zeph_miner.sh

--- setup_zeph_miner.sh
+++ setup_zeph_miner.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! command -v jq &> /dev/null; then
+    echo -e "\033[33m检测没有JQ正在安装...\033[0m"
+    if apt-get update -y > /dev/null && apt-get install jq -y > /dev/null; then
+        echo -e "\033[32m安装JQ成功\033[0m"
+    else
+        echo -e "\033[31m安装JQ失败\033[0m"
+        exit 1
+    fi
+fi
+
 VERSION=2.11
 
 # printing greetings
